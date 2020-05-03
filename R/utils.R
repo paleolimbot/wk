7acbf0b6cb86004e694560d2abbeee7fba71e1be
Dewey Dunnington <dewey@fishandwhistle.net>
2020-05-03 10:56:34 -0300

    add WKT class

---
+++ R/utils.R
@@ -0,0 +1,4 @@
+
+`%||%` <- function(x, y) {
+  if (is.null(x)) y else x
+}
---
+++ tests/testthat/test-wkt.R
@@ -0,0 +1,26 @@
+
+test_that("wkt class works", {
+  x <- wkt("POINT (40 10)")
+  expect_is(x, "wk_wkt")
+  expect_is(x, "wk_vctr")
+  expect_output(print(x), "wk_wkt")
+  expect_identical(as_wkt(x), x)
+
+  # make sure creation options get passed through for identity case
+  expect_identical(unclass(x), "POINT (40 10)")
+  expect_identical(unclass(as_wkt(x, trim = FALSE, precision = 3)), "POINT (40.000 10.000)")
+
+  expect_is(wkt(NA), "wk_wkt")
+
+  expect_error(new_wk_wkt(structure(character(), thing = "stuff")), "must be a character")
+  expect_error(new_wk_wkt(list()), "must be a character")
+  expect_error(wkt("NOPE"), "Encountered 1 parse problem")
+  expect_error(wkt(rep("NOPE", 10)), "Encountered 10 parse problems")
+
+  expect_is(x[1], "wk_wkt")
+  expect_identical(x[[1]], x[1])
+  expect_is(c(x, x), "wk_wkt")
+  expect_identical(rep(x, 2), c(x, x))
+  expect_identical(rep_len(x, 2), c(x, x))
+  expect_length(c(x, x), 2)
+})
