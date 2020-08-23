185902b49b0065afb55562a32c88ec67e0539cfa
Dewey Dunnington <dewey@fishandwhistle.net>
2020-08-23 17:02:01 -0300

    more C API stuff

--- inst/include/wk-v1.h
+++ inst/include/wk-v1.h
@@ -2,6 +2,8 @@
 #ifndef WK_V1_H_INCLUDED
 #define WK_V1_H_INCLUDED
 
+#include <Rinternals.h>
+
 #ifndef __cplusplus
 # include <stddef.h> /* for size_t definition */
 #else
@@ -51,6 +53,7 @@ typedef struct {
 } WKV1_GeometryMeta;
 
 typedef struct {
+  int WKAPIVersion = 1;
   char (*vectorStart)(WKV1_GeometryMeta* meta, void* userData);
   char (*featureStart)(WKV1_GeometryMeta* meta, size_t nFeatures, size_t featureId, void* userData);
   char (*nullFeature)(WKV1_GeometryMeta* meta, size_t nFeatures, size_t featureId, void* userData);
@@ -60,17 +63,16 @@ typedef struct {
   char (*ringEnd)(WKV1_GeometryMeta* meta, unsigned int nRings, unsigned int ringId, void* userData);
   char (*geometryEnd)(WKV1_GeometryMeta* meta, unsigned int nParts, unsigned int partId, void* userData);
   char (*featureEnd)(WKV1_GeometryMeta* meta, size_t nFeatures, size_t featureId, void* userData);
-  char (*vectorEnd)(WKV1_GeometryMeta* meta, void* userData);
-  void (*finalize)(void* userData);
-  char lastErrorMessage[WKV1_BUFFER_SIZE];
+  SEXP (*vectorEnd)(WKV1_GeometryMeta* meta, void* userData);
+  void* userData = NULL;
 } WKV1_Handler;
 
-inline char WKV1_handler_void_vector(WKV1_GeometryMeta* meta, void* userData) {
+inline char WKV1_handler_void_vector_start(WKV1_GeometryMeta* meta, void* userData) {
   return 0;
 }
 
-inline char WKV1_handler_void_null(WKV1_GeometryMeta* meta, size_t nFeatures, size_t featureId, void* userData) {
-  return 0;
+inline SEXP WKV1_handler_void_vector_end(WKV1_GeometryMeta* meta, void* userData) {
+  return R_NilValue;
 }
 
 inline char WKV1_handler_void_feature(WKV1_GeometryMeta* meta, size_t nFeatures, size_t featureId, void* userData) {
@@ -81,7 +83,7 @@ inline char WKV1_handler_void_geometry(WKV1_GeometryMeta* meta, unsigned int nPa
   return 0;
 }
 
-inline char WKV1_handler_void_ring(WKV1_GeometryMeta* meta, unsigned int nParts, unsigned int partId, void* userData) {
+inline char WKV1_handler_void_ring(WKV1_GeometryMeta* meta, unsigned int nRings, unsigned int ringId, void* userData) {
   return 0;
 }
 
@@ -89,28 +91,229 @@ inline char WKV1_handler_void_coord(WKV1_GeometryMeta* meta, WKV1_Coord coord, u
   return 0;
 }
 
-inline void WKV1_handler_void_finalize(void* userData) {}
+inline WKV1_Handler* WKV1_handler_create_void() {
+  WKV1_Handler* handler = (WKV1_Handler*) malloc(sizeof(WKV1_Handler));
 
-inline WKV1_Handler WKV1_handler_create_void() {
-  WKV1_Handler handler;
-  handler.vectorStart = &WKV1_handler_void_vector;
-  handler.vectorEnd = &WKV1_handler_void_vector;
+  handler->vectorStart = &WKV1_handler_void_vector_start;
+  handler->vectorEnd = &WKV1_handler_void_vector_end;
 
-  handler.featureStart = &WKV1_handler_void_feature;
-  handler.nullFeature = &WKV1_handler_void_null;
-  handler.featureEnd = &WKV1_handler_void_feature;
+  handler->featureStart = &WKV1_handler_void_feature;
+  handler->nullFeature = &WKV1_handler_void_feature;
+  handler->featureEnd = &WKV1_handler_void_feature;
 
-  handler.geometryStart = &WKV1_handler_void_geometry;
-  handler.geometryEnd = &WKV1_handler_void_geometry;
+  handler->geometryStart = &WKV1_handler_void_geometry;
+  handler->geometryEnd = &WKV1_handler_void_geometry;
 
-  handler.ringStart = &WKV1_handler_void_ring;
-  handler.ringEnd = &WKV1_handler_void_ring;
+  handler->ringStart = &WKV1_handler_void_ring;
+  handler->ringEnd = &WKV1_handler_void_ring;
 
-  handler.coord = &WKV1_handler_void_coord;
-
-  handler.finalize = &WKV1_handler_void_finalize;
+  handler->coord = &WKV1_handler_void_coord;
 
   return handler;
 }
+
+inline void WKV1_handler_destroy(WKV1_Handler* handler) {
+  free(handler);
+}
+
+#ifdef __cplusplus
+
+#include <string.h>
+
+class WKV1_HandlerClass {
+public:
+  char vectorStart(WKV1_GeometryMeta* meta) {
+    return 0;
+  }
+
+  char featureStart(WKV1_GeometryMeta* meta, size_t nFeatures, size_t featureId) {
+    return 0;
+  }
+
+  char nullFeature(WKV1_GeometryMeta* meta, size_t nFeatures, size_t featureId) {
+    return 0;
+  }
+
+  char geometryStart(WKV1_GeometryMeta* meta, unsigned int nParts, unsigned int partId) {
+    return 0;
+  }
+
+  char ringStart(WKV1_GeometryMeta* meta, unsigned int nRings, unsigned int ringId) {
+    return 0;
+  }
+
+  char coord(WKV1_GeometryMeta* meta, WKV1_Coord coord, unsigned int nCoords, unsigned int coordId) {
+    return 0;
+  }
+
+  char ringEnd(WKV1_GeometryMeta* meta, unsigned int nRings, unsigned int ringId) {
+    return 0;
+  }
+
+  char geometryEnd(WKV1_GeometryMeta* meta, unsigned int nParts, unsigned int partId) {
+    return 0;
+  }
+
+  char featureEnd(WKV1_GeometryMeta* meta, size_t nFeatures, size_t featureId) {
+    return 0;
+  }
+
+  SEXP vectorEnd(WKV1_GeometryMeta* meta) {
+    return R_NilValue;
+  }
+
+  std::string lastErrorMessage() {
+    return lastErrorMessage_;
+  }
+
+  void setLastErrorMesssage(std::string message) {
+    this->lastErrorMessage_ = message;
+  }
+
+private:
+  std::string lastErrorMessage_;
+};
+
+
+template <class HandlerType>
+class WKV1_HandlerFactory {
+public:
+
+  static WKV1_Handler* c_handler(HandlerType* userData) {
+    WKV1_Handler* handler = WKV1_handler_create_void();
+    handler->userData = userData;
+
+    handler->vectorStart = &vectorStart;
+    handler->vectorEnd = &vectorEnd;
+
+    handler->featureStart = &featureStart;
+    handler->nullFeature = &nullFeature;
+    handler->featureEnd = &featureEnd;
+
+    handler->geometryStart = &geometryStart;
+    handler->geometryEnd = &geometryEnd;
+
+    handler->ringStart = &ringStart;
+    handler->ringEnd = &ringEnd;
+
+    handler->coord = &coord;
+
+    return handler;
+  }
+
+  static SEXP sexp_handler(HandlerType* userData) {
+    WKV1_Handler* handler = c_handler(userData);
+    SEXP xptr = R_MakeExternalPtr(handler, R_NilValue, R_NilValue);
+    R_RegisterCFinalizerEx(xptr, &deleteUserDataXPtr, TRUE);
+    return xptr;
+  }
+
+private:
+  static void deleteUserDataXPtr(SEXP xptr) {
+    deleteUserData(R_ExternalPtrAddr(xptr));
+  }
+
+  static void deleteUserData(HandlerType* userData) {
+    if (userData != NULL) {
+      delete userData;
+    }
+  }
+
+  static char vectorStart(WKV1_GeometryMeta* meta, HandlerType* userData) {
+    try {
+      return userData->vectorStart(meta);
+    } catch (std::exception& e) {
+      userData->setLastErrorMessage(e.what());
+      return 1;
+    }
+  }
+
+  static char featureStart(WKV1_GeometryMeta* meta, size_t nFeatures, size_t featureId, HandlerType* userData) {
+    try {
+      return userData->featureStart(meta, nFeatures, featureId);
+    } catch (std::exception& e) {
+      userData->setLastErrorMessage(e.what());
+      return 1;
+    }
+  }
+
+  static char nullFeature(WKV1_GeometryMeta* meta, size_t nFeatures, size_t featureId, HandlerType* userData) {
+    try {
+      return userData->nullFeature(meta, nFeatures, featureId);
+    } catch (std::exception& e) {
+      userData->setLastErrorMessage(e.what());
+      return 1;
+    }
+  }
+
+  static char geometryStart(WKV1_GeometryMeta* meta, unsigned int nParts, unsigned int partId, HandlerType* userData) {
+    try {
+      return userData->geometryStart(meta, nParts, partId);
+    } catch (std::exception& e) {
+      userData->setLastErrorMessage(e.what());
+      return 1;
+    }
+  }
+
+  static char ringStart(WKV1_GeometryMeta* meta, unsigned int nRings, unsigned int ringId, HandlerType* userData) {
+    try {
+      return userData->ringStart(meta, nRings, ringId);
+    } catch (std::exception& e) {
+      userData->setLastErrorMessage(e.what());
+      return 1;
+    }
+  }
+
+  static char coord(WKV1_GeometryMeta* meta, WKV1_Coord coord, unsigned int nCoords, unsigned int coordId, HandlerType* userData) {
+    try {
+      return userData->coord(meta, coord, nCoords, coordId);
+    } catch (std::exception& e) {
+      userData->setLastErrorMessage(e.what());
+      return 1;
+    }
+  }
+
+  static char ringEnd(WKV1_GeometryMeta* meta, unsigned int nRings, unsigned int ringId, HandlerType* userData) {
+    try {
+      return userData->ringEnd(meta, nRings, ringId);
+    } catch (std::exception& e) {
+      userData->setLastErrorMessage(e.what());
+      return 1;
+    }
+  }
+
+  static char geometryEnd(WKV1_GeometryMeta* meta, unsigned int nParts, unsigned int partId, HandlerType* userData) {
+    try {
+      return userData->geometryEnd(meta, nParts, partId);
+    } catch (std::exception& e) {
+      userData->setLastErrorMessage(e.what());
+      return 1;
+    }
+  }
+
+  static char featureEnd(WKV1_GeometryMeta* meta, size_t nFeatures, size_t featureId, HandlerType* userData) {
+    try {
+      return userData->featureEnd(meta, nFeatures, featureId);
+    } catch (std::exception& e) {
+      userData->setLastErrorMessage(e.what());
+      return 1;
+    }
+  }
+
+  SEXP vectorEnd(WKV1_GeometryMeta* meta, HandlerType* userData) {
+    try {
+      return userData->vectorEnd(meta);
+    } catch (std::exception& e) {
+      SEXP tryError = PROTECT(Rf_allocVector(STRSXP, 1));
+      SET_STRING_ELT(tryError, 0, Rf_mkChar(e.what()));
+      Rf_setAttrib(tryError, Rf_install("class"), Rf_mkString("wk_v1_error"));
+      userData->setLastErrorMessage(e.what());
+      UNPROTECT(1);
+      return tryError;
+    }
+  }
+};
+
+#endif
 
 #endif
