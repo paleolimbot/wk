68243163a50d36efde2485f5ace661a985e92ba8
Dewey Dunnington <dewey@fishandwhistle.net>
2020-08-24 21:25:06 -0300

    less noisy tests

--- tests/testthat/test-handler.R
+++ tests/testthat/test-handler.R
@@ -8,5 +8,4 @@ test_that("void and debug handlers can be created", {
 
 test_that("wk_handlers have a default print method", {
   expect_output(print(wk_void_handler()), "wk_void_handler")
-  expect_is(print(wk_void_handler()), "wk_void_handler")
 })
