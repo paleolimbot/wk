7f856bdfc84ecac050fae0feee5b9285ae07e4a9
Dewey Dunnington <dewey@fishandwhistle.net>
2021-01-01 21:43:05 -0400

    no more finishers/attributes on handlers

--- R/handler.R
+++ R/handler.R
@@ -4,7 +4,7 @@
 #' @param handler_ptr An external pointer to a newly created WK handler
 #' @param handler An object created with [new_wk_handler()].
 #' @param subclass The handler subclass
-#' @param finisher,result A function called on the `result` of the handler.
+#' @param result A function called on the `result` of the handler.
 #' @param precision If `trim` is `TRUE`, the total number of significant digits to keep
 #'   for each result or the number of digits after the decimal place otherwise.
 #' @param trim Use `FALSE` to keep trailing zeroes after the decimal place.
@@ -14,13 +14,13 @@
 #' @export
 #'
 wk_void_handler <- function() {
-  new_wk_handler(.Call(wk_c_handler_void_new), "wk_void_handler", finisher = wk_finish_invisible)
+  new_wk_handler(.Call(wk_c_handler_void_new), "wk_void_handler")
 }
 
 #' @rdname wk_void_handler
 #' @export
 wk_debug_handler <- function() {
-  new_wk_handler(.Call(wk_c_handler_debug_new), "wk_debug_handler", finisher = wk_finish_invisible)
+  new_wk_handler(.Call(wk_c_handler_debug_new), "wk_debug_handler")
 }
 
 #' @rdname wk_void_handler
@@ -43,9 +43,9 @@ wkb_writer <- function() {
 
 #' @rdname wk_void_handler
 #' @export
-new_wk_handler <- function(handler_ptr, subclass = character(), finisher = wk_finish_default) {
-  stopifnot(is.function(finisher), typeof(handler_ptr) == "externalptr")
-  structure(handler_ptr, class = union(subclass, "wk_handler"), finisher = finisher)
+new_wk_handler <- function(handler_ptr, subclass = character()) {
+  stopifnot(typeof(handler_ptr) == "externalptr")
+  structure(handler_ptr, class = union(subclass, "wk_handler"))
 }
 
 #' @rdname wk_void_handler
@@ -72,32 +72,16 @@ as_wk_handler.function <- function(handler, ...) {
 
 #' @rdname wk_void_handler
 #' @export
-wk_finish_default <- function(result, x) {
-  if (inherits(result, "wk_error_sentinel")) {
-    stop(result$message, call. = FALSE)
-  } else {
-    result
-  }
-}
-
-#' @rdname wk_void_handler
-#' @export
-wk_finish_invisible <- function(result, x) {
-  invisible(wk_finish_default(result, x))
-}
-
-#' @rdname wk_void_handler
-#' @export
 handle_wkb <- function(x, handler, ...) {
   handler <- as_wk_handler(handler, ...)
-  attr(handler, "finisher")(.Call(wk_c_read_wkb, as_wkb(x), handler))
+  .Call(wk_c_read_wkb, as_wkb(x), handler)
 }
 
 #' @rdname wk_void_handler
 #' @export
 handle_wkt <- function(x, handler, ...) {
   handler <- as_wk_handler(handler, ...)
-  attr(handler, "finisher")(wk_cpp_handle_wkt(as_wkt(x), handler))
+  wk_cpp_handle_wkt(as_wkt(x), handler)
 }
 
 #' @export
