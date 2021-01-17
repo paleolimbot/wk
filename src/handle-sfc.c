207599b22b8a9a46b05d8a61c78bb4c464ff1ec1
Dewey Dunnington <dewey@fishandwhistle.net>
2021-01-16 20:24:50 -0400

    restructure for better re-use potential for multi* geoms in sfc reader

--- src/handle-sfc.c
+++ src/handle-sfc.c
@@ -14,9 +14,9 @@
   if (result == WK_ABORT_FEATURE) continue; else if (result == WK_ABORT) break
 
 int wk_sfc_read_sfg(SEXP x, wk_handler_t* handler, uint32_t part_id);
-int wk_sfc_read_point(SEXP x, wk_handler_t* handler, uint32_t part_id);
-int wk_sfc_read_linestring(SEXP x, wk_handler_t* handler, uint32_t part_id);
-int wk_sfc_read_polygon(SEXP x, wk_handler_t* handler, uint32_t part_id);
+int wk_sfc_read_point(SEXP x, wk_handler_t* handler, wk_meta_t* meta, uint32_t part_id);
+int wk_sfc_read_linestring(SEXP x, wk_handler_t* handler, wk_meta_t* meta, uint32_t part_id);
+int wk_sfc_read_polygon(SEXP x, wk_handler_t* handler, wk_meta_t* meta, uint32_t part_id);
 
 SEXP wk_c_read_sfc_impl(SEXP data, wk_handler_t* handler) {
     R_xlen_t n_features = Rf_xlength(data);
@@ -52,21 +52,7 @@ SEXP wk_c_read_sfc(SEXP data, SEXP handler_xptr) {
     return wk_handler_run_xptr(&wk_c_read_sfc_impl, data, handler_xptr);
 }
 
-int wk_sfc_read_sfg(SEXP x, wk_handler_t* handler, uint32_t part_id) {
-    if (Rf_inherits(x, "POINT")) {
-        return wk_sfc_read_point(x, handler, part_id);
-    } else if (Rf_inherits(x, "LINESTRING")) {
-        return wk_sfc_read_linestring(x, handler, part_id);
-    } else if (Rf_inherits(x, "POLYGON")) {
-        return wk_sfc_read_polygon(x, handler, part_id);
-    }  else if (Rf_inherits(x, "sfg")) {
-        Rf_error("Unsupported sfg type");
-    } else {
-        Rf_error("Element of sfc list must inherit from 'sfg'");
-    }
-}
-
-void wk_meta_from_sfg(SEXP x, wk_meta_t* meta) {
+void wk_update_meta_from_sfg(SEXP x, wk_meta_t* meta) {
     if (Rf_inherits(x, "XY")) {
         // don't need to do anything here; default meta is xy
     } else if (Rf_inherits(x, "XYZ")) {
@@ -76,79 +62,91 @@ void wk_meta_from_sfg(SEXP x, wk_meta_t* meta) {
     } else if (Rf_inherits(x, "XYZM")) {
         meta->flags |= WK_FLAG_HAS_Z;
         meta->flags |= WK_FLAG_HAS_M;
-    } else {
+    } else if (Rf_inherits(x, "sfg")) {
         Rf_error("Can't guess dimensions from class of 'sfg'");
     }
 }
 
-int wk_sfc_read_point(SEXP x, wk_handler_t* handler, uint32_t part_id) {
-    int result;
+int wk_sfc_read_sfg(SEXP x, wk_handler_t* handler, uint32_t part_id) {
     wk_meta_t meta;
-    WK_META_RESET(meta, WK_POINT);
-    meta.size = 0;
-    wk_meta_from_sfg(x, &meta);
+    WK_META_RESET(meta, WK_GEOMETRY);
+    wk_update_meta_from_sfg(x, &meta);
+
+    if (Rf_inherits(x, "POINT")) {
+        return wk_sfc_read_point(x, handler, &meta, part_id);
+    } else if (Rf_inherits(x, "LINESTRING")) {
+        return wk_sfc_read_linestring(x, handler, &meta, part_id);
+    } else if (Rf_inherits(x, "POLYGON")) {
+        return wk_sfc_read_polygon(x, handler, &meta, part_id);
+    }  else if (Rf_inherits(x, "sfg")) {
+        Rf_error("Unsupported sfg type");
+    } else {
+        Rf_error("Element of sfc list must inherit from 'sfg'");
+    }
+}
+
+int wk_sfc_read_point(SEXP x, wk_handler_t* handler, wk_meta_t* meta, uint32_t part_id) {
+    int result;
+    meta->geometry_type = WK_POINT;
+    meta->size = 0;
 
     int coord_size = Rf_length(x);
     int all_na = 1;
     for (int i = 0; i < coord_size; i++) {
         if (!ISNA(REAL(x)[i])) {
             all_na = 0;
-            meta.size = 1;
+            meta->size = 1;
             break;
         }
     }
     
-    HANDLE_OR_RETURN(handler->geometry_start(&meta, part_id, handler->handler_data));
+    HANDLE_OR_RETURN(handler->geometry_start(meta, part_id, handler->handler_data));
 
-    if (meta.size) {
+    if (meta->size) {
         wk_coord_t coord;
         memcpy(coord.v, REAL(x), sizeof(double) * coord_size);
-        HANDLE_OR_RETURN(handler->coord(&meta, coord, 0, handler->handler_data));
+        HANDLE_OR_RETURN(handler->coord(meta, coord, 0, handler->handler_data));
     }
 
-    HANDLE_OR_RETURN(handler->geometry_end(&meta, part_id, handler->handler_data));
+    HANDLE_OR_RETURN(handler->geometry_end(meta, part_id, handler->handler_data));
     return WK_CONTINUE;
 }
 
-int wk_sfc_read_linestring(SEXP x, wk_handler_t* handler, uint32_t part_id) {
+int wk_sfc_read_linestring(SEXP x, wk_handler_t* handler, wk_meta_t* meta, uint32_t part_id) {
     int result;
-    wk_meta_t meta;
-    WK_META_RESET(meta, WK_LINESTRING);
-    meta.size = Rf_nrows(x);
+    meta->geometry_type = WK_LINESTRING;
+    meta->size = Rf_nrows(x);
     int coord_size = Rf_ncols(x);
-    wk_meta_from_sfg(x, &meta);
     
-    HANDLE_OR_RETURN(handler->geometry_start(&meta, part_id, handler->handler_data));
+    HANDLE_OR_RETURN(handler->geometry_start(meta, part_id, handler->handler_data));
 
     wk_coord_t coord;
     double* coords = REAL(x);
-    for (uint32_t i = 0; i < meta.size; i++) {
+    for (uint32_t i = 0; i < meta->size; i++) {
         for (int j = 0; j < coord_size; j++) {
-             coord.v[j] = coords[j * meta.size + i];
+             coord.v[j] = coords[j * meta->size + i];
         }
-        HANDLE_OR_RETURN(handler->coord(&meta, coord, i, handler->handler_data));
+        HANDLE_OR_RETURN(handler->coord(meta, coord, i, handler->handler_data));
     }
 
-    HANDLE_OR_RETURN(handler->geometry_end(&meta, part_id, handler->handler_data));
+    HANDLE_OR_RETURN(handler->geometry_end(meta, part_id, handler->handler_data));
     return WK_CONTINUE;
 }
 
-int wk_sfc_read_polygon(SEXP x, wk_handler_t* handler, uint32_t part_id) {
+int wk_sfc_read_polygon(SEXP x, wk_handler_t* handler, wk_meta_t* meta, uint32_t part_id) {
     int result;
-    wk_meta_t meta;
-    WK_META_RESET(meta, WK_POLYGON);
-    meta.size = Rf_xlength(x);
-    wk_meta_from_sfg(x, &meta);
+    meta->geometry_type = WK_POLYGON;
+    meta->size = Rf_xlength(x);
     
-    HANDLE_OR_RETURN(handler->geometry_start(&meta, part_id, handler->handler_data));
+    HANDLE_OR_RETURN(handler->geometry_start(meta, part_id, handler->handler_data));
 
     SEXP ring;
-    for (uint32_t ring_id = 0; ring_id < meta.size; ring_id++) {
+    for (uint32_t ring_id = 0; ring_id < meta->size; ring_id++) {
         ring = VECTOR_ELT(x, ring_id);
         uint32_t ring_size = Rf_nrows(ring);
         int coord_size = Rf_ncols(ring);
         
-        HANDLE_OR_RETURN(handler->ring_start(&meta, meta.size, ring_id, handler->handler_data));
+        HANDLE_OR_RETURN(handler->ring_start(meta, meta->size, ring_id, handler->handler_data));
 
         wk_coord_t coord;
         double* coords = REAL(ring);
@@ -156,12 +154,12 @@ int wk_sfc_read_polygon(SEXP x, wk_handler_t* handler, uint32_t part_id) {
             for (int j = 0; j < coord_size; j++) {
                 coord.v[j] = coords[j * ring_size + i];
             }
-            HANDLE_OR_RETURN(handler->coord(&meta, coord, i, handler->handler_data));
+            HANDLE_OR_RETURN(handler->coord(meta, coord, i, handler->handler_data));
         }
 
-        HANDLE_OR_RETURN(handler->ring_end(&meta, meta.size, ring_id, handler->handler_data));
+        HANDLE_OR_RETURN(handler->ring_end(meta, meta->size, ring_id, handler->handler_data));
     }
 
-    HANDLE_OR_RETURN(handler->geometry_end(&meta, part_id, handler->handler_data));
+    HANDLE_OR_RETURN(handler->geometry_end(meta, part_id, handler->handler_data));
     return WK_CONTINUE;
 }
