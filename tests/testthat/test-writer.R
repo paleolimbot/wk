9f35024da2142213ca3d77d86cd871fcbfd1314f
Dewey Dunnington <dewey@fishandwhistle.net>
2021-01-24 20:46:22 -0400

    add test for wk_writer()

--- tests/testthat/test-writer.R
+++ tests/testthat/test-writer.R
@@ -1,4 +1,11 @@
 
+test_that("the wk_writer() generic resolves correct handler", {
+  expect_is(wk_writer(wkt()), "wk_wkt_writer")
+  expect_is(wk_writer(wkb()), "wk_wkb_writer")
+  expect_is(wk_writer(xy()), "wk_xyzm_writer")
+  expect_is(wk_writer(structure(list(), class = "sfc")), "wk_sfc_writer")
+})
+
 test_that("wkt_writer() works", {
   wkt_good <- as_wkt(
     c(
