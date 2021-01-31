b0d23c68afee1eefb1de9213c93ccadc3d208510
Dewey Dunnington <dewey@fishandwhistle.net>
2021-01-31 13:21:10 -0400

    traslate() for data frames

--- R/compat-data-frame.R
+++ R/compat-data-frame.R
@@ -1,24 +1,71 @@
 
-#' @rdname wk_handle
+#' Use data.frame with wk
+#'
+#' @inheritParams wk_handle
+#' @inheritParams wk_translate
+#' @param .env Passed to [getS3method()], which is used to find
+#'   the column in a [data.frame()] for which a [wk_handle()]
+#'   method is defined.
+#'
 #' @export
+#'
+#' @examples
+#' wk_handle(data.frame(a = wkt("POINT (0 1)")), wkb_writer())
+#' wk_translate(wkt("POINT (0 1)"), data.frame(col_name = wkb()))
+#' wk_translate(data.frame(a = wkt("POINT (0 1)")), data.frame(wkb()))
+#'
 wk_handle.data.frame <- function(handleable, handler, ..., .env = parent.frame()) {
   col <- handleable_column_name(handleable)
   wk_handle(handleable[[col]], handler, ...)
 }
 
-handleable_column_name <- function(df, .env = parent.frame()) {
-  has_handle_method <- vapply(df, has_wk_handle_method, FUN.VALUE = logical(1), .env = .env)
+#' @rdname wk_handle.data.frame
+#' @export
+wk_translate.data.frame <- function(handleable, to, ..., .env = parent.frame()) {
+  col <- writable_column_name(to)
+  col_value <- wk_translate(handleable, to[[col]], ...)
 
-  if (sum(has_handle_method) != 1) {
+  if (inherits(handleable, "data.frame")) {
+    handleable_col <- handleable_column_name(handleable)
+    handleable[handleable_col] <- list(col_value)
+    handleable
+  } else {
+    df_raw <- list(col_value)
+    names(df_raw) <- col
+    new_data_frame(df_raw)
+  }
+}
+
+handleable_column_name <- function(df, .env = parent.frame()) {
+  has_method <- vapply(df, has_wk_handle_method, FUN.VALUE = logical(1), .env = .env)
+
+  if (sum(has_method) != 1) {
     stop(
       "To be used with wk_handle(), a data.frame must have exactly one handleable column.",
       call. = FALSE
     )
   }
 
-  names(df)[has_handle_method]
+  names(df)[has_method]
 }
 
 has_wk_handle_method <- function(x, .env = parent.frame()) {
-  !is.null(getS3method("wk_handle", class(x), optional = TRUE, envir = .env))
+  !is.null(utils::getS3method("wk_handle", class(x), optional = TRUE, envir = .env))
+}
+
+writable_column_name <- function(df, .env = parent.frame()) {
+  has_method <- vapply(df, has_wk_writer_method, FUN.VALUE = logical(1), .env = .env)
+
+  if (sum(has_method) != 1) {
+    stop(
+      "To be used with wk_translate(), a data.frame must have exactly one wk_writer() column.",
+      call. = FALSE
+    )
+  }
+
+  names(df)[has_method]
+}
+
+has_wk_writer_method <- function(x, .env = parent.frame()) {
+  !is.null(utils::getS3method("wk_writer", class(x), optional = TRUE, envir = .env))
 }
--- tests/testthat/test-compat-data-frame.R
+++ tests/testthat/test-compat-data-frame.R
@@ -6,3 +6,18 @@ test_that("wk_handle() works for data.frame", {
     wk_handle(wkt("POINT (0 1)"), wkb_writer())
   )
 })
+
+test_that("wk_translate() works for data.frame", {
+  expect_identical(
+    wk_translate(as_wkb("POINT (1 2)"), data.frame(a = wkt())),
+    data.frame(a = wkt("POINT (1 2)"))
+  )
+
+  expect_identical(
+    wk_translate(
+      data.frame(a = as_wkb("POINT (1 2)")),
+      data.frame(a = wkt())
+    ),
+    data.frame(a = wkt("POINT (1 2)"))
+  )
+})
