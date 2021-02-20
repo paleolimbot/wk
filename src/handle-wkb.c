2e2bbb661d851bafae94747e1ca4b9e9d8c4dfa1
Dewey Dunnington <dewey@fishandwhistle.net>
2021-02-19 22:01:16 -0400

    more consistent method signatures/names

--- src/handle-wkb.c
+++ src/handle-wkb.c
@@ -25,16 +25,17 @@ typedef struct {
     char error_buf[1024];
 } wkb_reader_t;
 
-void wkb_parse_geometry_type(uint32_t geometry_type, wk_meta_t* meta);
 unsigned char wkb_platform_endian();
 void memcpyrev(void* dst, unsigned char* src, size_t n);
 
-void wkb_set_errorf(wkb_reader_t* reader, const char* error_buf, ...);
+void wkb_read_set_errorf(wkb_reader_t* reader, const char* error_buf, ...);
 int wkb_read_geometry(wkb_reader_t* reader, uint32_t part_id);
-int wkb_read_endian(wkb_reader_t* reader);
+int wkb_read_endian(wkb_reader_t* reader, unsigned char* value);
+void wkb_parse_geometry_type(uint32_t geometry_type, wk_meta_t* meta);
+
 int wkb_read_uint(wkb_reader_t* reader, uint32_t* value);
 int wkb_read_coordinates(wkb_reader_t* reader, const wk_meta_t* meta, uint32_t n_coords);
-int wkb_check_buffer(wkb_reader_t* reader, size_t bytes);
+int wkb_read_check_buffer(wkb_reader_t* reader, size_t bytes);
 
 #define HANDLE_OR_RETURN(expr)                                 \
     result = expr;                                             \
@@ -71,7 +72,7 @@ void wkb_parse_geometry_type(uint32_t geometry_type, wk_meta_t* meta) {
     }
 }
 
-void wkb_set_errorf(wkb_reader_t* reader, const char* error_buf, ...) {
+void wkb_read_set_errorf(wkb_reader_t* reader, const char* error_buf, ...) {
     reader->error_code = WK_DEFAULT_ERROR_CODE;
     va_list args;
     va_start(args, error_buf);
@@ -81,7 +82,10 @@ void wkb_set_errorf(wkb_reader_t* reader, const char* error_buf, ...) {
 
 int wkb_read_geometry(wkb_reader_t* reader, uint32_t part_id) {
     int result;
-    HANDLE_OR_RETURN(wkb_read_endian(reader));
+
+    unsigned char endian;
+    HANDLE_OR_RETURN(wkb_read_endian(reader, &endian));
+    reader->swap_endian = endian != wkb_platform_endian();
 
     uint32_t geometry_type;
     HANDLE_OR_RETURN(wkb_read_uint(reader, &geometry_type));
@@ -89,8 +93,9 @@ int wkb_read_geometry(wkb_reader_t* reader, uint32_t part_id) {
     wk_meta_t meta;
     WK_META_RESET(meta, WK_GEOMETRY);
     wkb_parse_geometry_type(geometry_type, &meta);
+    
     if (meta.geometry_type < WK_POINT || meta.geometry_type > WK_GEOMETRYCOLLECTION) {
-        wkb_set_errorf(reader, "Unrecognized geometry type code '%d'", meta.geometry_type);
+        wkb_read_set_errorf(reader, "Unrecognized geometry type code '%d'", meta.geometry_type);
         return WK_ABORT_FEATURE;
     }
 
@@ -130,51 +135,44 @@ int wkb_read_geometry(wkb_reader_t* reader, uint32_t part_id) {
         break;
     default:
         // this should never be reached as the type is checked above
-        wkb_set_errorf(reader, "Unrecognized geometry type code '%d'", meta.geometry_type); // # nocov
+        wkb_read_set_errorf(reader, "Unrecognized geometry type code '%d'", meta.geometry_type); // # nocov
         return WK_ABORT_FEATURE; // # nocov
     }
 
     return reader->handler->geometry_end(&meta, part_id, reader->handler->handler_data);
 }
 
-int wkb_read_endian(wkb_reader_t* reader) {
-    if (wkb_check_buffer(reader, 1) == WK_CONTINUE) {
-        unsigned char value;
-        memcpy(&value, &(reader->buffer[reader->offset]), 1);
-        reader->offset += 1;
-        reader->swap_endian = value != wkb_platform_endian();
-        return WK_CONTINUE;
-    } else {
-        return WK_ABORT_FEATURE;
-    }
+int wkb_read_endian(wkb_reader_t* reader, unsigned char* value) {
+    int result;
+    HANDLE_OR_RETURN(wkb_read_check_buffer(reader, sizeof(unsigned char)));
+
+    memcpy(value, &(reader->buffer[reader->offset]), sizeof(unsigned char));
+    reader->offset += sizeof(unsigned char);
+    return WK_CONTINUE;
 }
 
 int wkb_read_uint(wkb_reader_t* reader, uint32_t* value) {
-    if (wkb_check_buffer(reader, sizeof(uint32_t)) == WK_CONTINUE) {
-        if (reader->swap_endian) {
-            memcpyrev(value, &(reader->buffer[reader->offset]), sizeof(uint32_t));
-            reader->offset += sizeof(uint32_t);
-        } else {
-            memcpy(value, &(reader->buffer[reader->offset]), sizeof(uint32_t));
-            reader->offset += sizeof(uint32_t);
-        }
+    int result;
+    HANDLE_OR_RETURN(wkb_read_check_buffer(reader, sizeof(uint32_t)));
 
-        return WK_CONTINUE;
+    if (reader->swap_endian) {
+        memcpyrev(value, &(reader->buffer[reader->offset]), sizeof(uint32_t));
+        reader->offset += sizeof(uint32_t);
     } else {
-        return WK_ABORT_FEATURE;
+        memcpy(value, &(reader->buffer[reader->offset]), sizeof(uint32_t));
+        reader->offset += sizeof(uint32_t);
     }
+
+    return WK_CONTINUE;
 }
 
 int wkb_read_coordinates(wkb_reader_t* reader, const wk_meta_t* meta, uint32_t n_coords) {
-    int n_dim = 2 + ((meta->flags & WK_FLAG_HAS_Z )!= 0) + ((meta->flags & WK_FLAG_HAS_M) != 0);
+    int n_dim = 2 + ((meta->flags & WK_FLAG_HAS_Z) != 0) + ((meta->flags & WK_FLAG_HAS_M) != 0);
     size_t coord_size = n_dim * sizeof(double);
-
-    if (wkb_check_buffer(reader, coord_size * n_coords) != WK_CONTINUE) {
-        return WK_ABORT_FEATURE;
-    }
-
     wk_coord_t coord;
     int result;
+
+    HANDLE_OR_RETURN(wkb_read_check_buffer(reader, coord_size * n_coords));
 
     if (reader->swap_endian) {
         for (uint32_t i = 0; i < n_coords; i++) {
@@ -196,11 +194,11 @@ int wkb_read_coordinates(wkb_reader_t* reader, const wk_meta_t* meta, uint32_t n
     return WK_CONTINUE;
 }
 
-int wkb_check_buffer(wkb_reader_t* reader, size_t bytes) {
+int wkb_read_check_buffer(wkb_reader_t* reader, size_t bytes) {
     if ((reader->offset + bytes) <= reader->size) {
         return WK_CONTINUE;
     } else {
-        wkb_set_errorf(reader, "Unexpected end of buffer (%d/%d)", reader->offset + bytes, reader->size);
+        wkb_read_set_errorf(reader, "Unexpected end of buffer (%d/%d)", reader->offset + bytes, reader->size);
         return WK_ABORT_FEATURE;
     }
 }
