f9c44f87a22b46cf0d710d8a1cc78bde319729ce
Dewey Dunnington <dewey@fishandwhistle.net>
2021-02-21 20:13:44 -0400

    build with latest cpp11

--- src/cpp11.cpp
+++ src/cpp11.cpp
@@ -71,4 +71,5 @@ static const R_CallMethodDef CallEntries[] = {
 extern "C" void R_init_wk(DllInfo* dll){
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
+  R_forceSymbols(dll, TRUE);
 }
