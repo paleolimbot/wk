99155fa85003522ab31085f5b7455d9a8cfd7cda
Dewey Dunnington <dewey@fishandwhistle.net>
2021-02-28 20:24:04 -0400

    problems handler theoretically handles vectors of unknown length

--- src/problems-handler.c
+++ src/problems-handler.c
@@ -5,48 +5,75 @@
 #include "wk-v1.h"
 #include <stdlib.h>
 
+#define PROBLEMS_DEFAULT_SIZE 1024
+
 typedef struct {
     SEXP problems;
-    R_xlen_t last_feat_id;
+    R_xlen_t feat_id;
 } wk_problems_handler_t;
 
 int wk_problems_handler_vector_start(const wk_vector_meta_t* meta, void* handler_data) {
     wk_problems_handler_t* data = (wk_problems_handler_t*) handler_data;
 
-    if (meta->size == WK_VECTOR_SIZE_UNKNOWN) {
-        Rf_error("Can't handle vector of unknown size");
-    }
-
     if (data->problems != R_NilValue) {
         Rf_error("Destination vector was already allocated"); // # nocov
     }
 
-    data->problems = PROTECT(Rf_allocVector(STRSXP, meta->size));
-    R_xlen_t n_features = meta->size;
-    for (R_xlen_t i = 0; i < n_features; i++) {
-        SET_STRING_ELT(data->problems, i, NA_STRING);
+    R_xlen_t n_features;
+    if (meta->size == WK_VECTOR_SIZE_UNKNOWN) {
+        n_features = PROBLEMS_DEFAULT_SIZE;
+    } else {
+        n_features = meta->size;
     }
 
+    data->problems = PROTECT(Rf_allocVector(STRSXP, n_features));
     R_PreserveObject(data->problems);
     UNPROTECT(1);
+
+    data->feat_id = 0;
+
     return WK_CONTINUE;
 }
 
 int wk_problems_handler_feature_start(const wk_vector_meta_t* meta, R_xlen_t feat_id, void* handler_data) {
     wk_problems_handler_t* data = (wk_problems_handler_t*) handler_data;
-    data->last_feat_id = feat_id;
+    if (feat_id >= Rf_xlength(data->problems)) {
+        SEXP new_result = PROTECT(Rf_allocVector(STRSXP, Rf_xlength(data->problems) * 2 + 1));
+        for (R_xlen_t i = 0; i < Rf_xlength(data->problems); i++) {
+            SET_STRING_ELT(new_result, i, STRING_ELT(data->problems, i));
+        }
+        R_ReleaseObject(data->problems);
+        data->problems = new_result;
+        R_PreserveObject(data->problems);
+        UNPROTECT(1);
+    }
+
+    SET_STRING_ELT(data->problems, data->feat_id, NA_STRING);
+    data->feat_id++;
     return WK_CONTINUE;
 }
 
 int wk_problems_handler_error(const char* message, void* handler_data) {
     wk_problems_handler_t* data = (wk_problems_handler_t*) handler_data;
-    SET_STRING_ELT(data->problems, data->last_feat_id, Rf_mkCharCE(message, CE_UTF8));
+    SET_STRING_ELT(data->problems, data->feat_id - 1, Rf_mkCharCE(message, CE_UTF8));
     return WK_ABORT_FEATURE;
 }
 
 SEXP wk_problems_handler_vector_end(const wk_vector_meta_t* meta, void* handler_data) {
     wk_problems_handler_t* data = (wk_problems_handler_t*) handler_data;
-    return data->problems;
+
+    if (data->feat_id != Rf_xlength(data->problems)) {
+        SEXP new_result = PROTECT(Rf_allocVector(STRSXP, data->feat_id));
+        for (R_xlen_t i = 0; i < Rf_xlength(data->problems); i++) {
+            SET_STRING_ELT(new_result, i, STRING_ELT(data->problems, i));
+        }
+        R_ReleaseObject(data->problems);
+        data->problems = R_NilValue;
+        UNPROTECT(1);
+        return new_result;
+    } else {
+        return data->problems;
+    }
 }
 
 void wk_problems_handler_deinitialize(void* handler_data) {
@@ -80,7 +107,7 @@ SEXP wk_c_problems_handler_new() {
         Rf_error("Failed to alloc handler data"); // # nocov
     }
 
-    data->last_feat_id = 0;
+    data->feat_id = 0;
     data->problems = R_NilValue;
 
     SEXP xptr = wk_handler_create_xptr(handler, R_NilValue, R_NilValue);
