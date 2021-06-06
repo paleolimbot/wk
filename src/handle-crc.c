c64c48549d365c62c235157bd62e8e25dbbda75f
Dewey Dunnington <dewey@fishandwhistle.net>
2021-06-05 22:29:44 -0300

    ensure that wk_crc() is exported as a polygon with a closed loop (#66)

--- src/handle-crc.c
+++ src/handle-crc.c
@@ -22,12 +22,19 @@ int wk_crc_handle_single(wk_handler_t* handler, const wk_meta_t* meta,
     double coord[4];
     double angle;
 
-    for (int i = 0; i <= segs_per_circle; i++) {
+    for (int i = 0; i < segs_per_circle; i++) {
         angle = i / segs_per_circle * PI * 2.0;
         coord[0] = x + r * cos(angle);
         coord[1] = y + r * sin(angle);
         HANDLE_OR_RETURN(handler->coord(meta, coord, i, handler->handler_data));
     }
+
+    // re-export the first coordinate (i = 0) identically
+    // to ensure that the loops are closed with no floating-point error
+    angle = 0 / segs_per_circle * PI * 2.0;
+    coord[0] = x + r * cos(angle);
+    coord[1] = y + r * sin(angle);
+    HANDLE_OR_RETURN(handler->coord(meta, coord, segs_per_circle, handler->handler_data));
 
     return WK_CONTINUE;
 }
