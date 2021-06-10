88b1b973ad0410a11ec9f2c051e3a81ebb957c17
Dewey Dunnington <dewey@fishandwhistle.net>
2021-06-09 21:41:39 -0300

    fall back to `wkb()` as default for `wk_writer()` (#73)

--- R/writer.R
+++ R/writer.R
@@ -1,20 +1,35 @@
 
 #' Write geometry vectors
 #'
+#' When writing transformation functions, it is often useful to know which
+#' handler should be used to create a (potentially modified) version
+#' of an object. Some transformers (e.g., [wk_vertices()]) modify
+#' the geometry type of an object, in which case a generic writer is needed.
+#' This defaults to [wkb_writer()] because it is fast and can handle
+#' all geometry types.
+#'
 #' @inheritParams wk_handle
 #' @param precision If `trim` is `TRUE`, the total number of significant digits to keep
 #'   for each result or the number of digits after the decimal place otherwise.
 #' @param trim Use `FALSE` to keep trailing zeroes after the decimal place.
 #' @param endian Use 1 for little endian, 0 for big endian, or NA for
 #'   system endian.
+#' @param generic Use `TRUE` to obtain a writer that can write all geometry
+#'   types.
 #' @param buffer_size Control the initial buffer size used when writing WKB.
 #' @param ... Passed to the writer constructor.
 #'
 #' @return A [wk_handler][wk_handle].
 #' @export
 #'
-wk_writer <- function(handleable, ...) {
+wk_writer <- function(handleable, ..., generic = FALSE) {
   UseMethod("wk_writer")
+}
+
+#' @rdname wk_writer
+#' @export
+wk_writer.default <- function(handleable, ...) {
+  wkb_writer()
 }
 
 #' @rdname wk_writer
@@ -31,6 +46,6 @@ wk_writer.wk_wkb <- function(handleable, ...) {
 
 #' @rdname wk_writer
 #' @export
-wk_writer.wk_xy <- function(handleable, ...) {
-  xy_writer()
+wk_writer.wk_xy <- function(handleable, ..., generic = FALSE) {
+  if (generic) wkb_writer() else xy_writer()
 }
--- tests/testthat/test-writer.R
+++ tests/testthat/test-writer.R
@@ -3,5 +3,8 @@ test_that("the wk_writer() generic resolves correct handler", {
   expect_s3_class(wk_writer(wkt()), "wk_wkt_writer")
   expect_s3_class(wk_writer(wkb()), "wk_wkb_writer")
   expect_s3_class(wk_writer(xy()), "wk_xy_writer")
+  expect_s3_class(wk_writer(xy(), generic = TRUE), "wk_wkb_writer")
+  expect_s3_class(wk_writer(rct()), "wk_wkb_writer")
+  expect_s3_class(wk_writer(crc()), "wk_wkb_writer")
   expect_s3_class(wk_writer(structure(list(), class = "sfc")), "wk_sfc_writer")
 })
