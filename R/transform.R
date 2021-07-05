00fd3c9bea5139aebabb1b2a001bdecccc62ba04
Dewey Dunnington <dewey@fishandwhistle.net>
2021-07-05 09:26:31 -0300

    export transform methods

--- R/transform.R
+++ R/transform.R
@@ -18,6 +18,8 @@ wk_transform <- function(handleable, trans, ...) {
   wk_restore(handleable, result, ...)
 }
 
+#' @rdname wk_transform
+#' @export
 wk_transform_filter <- function(handler, trans) {
   new_wk_handler(
     .Call(wk_c_trans_filter_new, as_wk_handler(handler), as_wk_trans(trans)),
@@ -25,18 +27,34 @@ wk_transform_filter <- function(handler, trans) {
   )
 }
 
+#' Generic transform class
+#'
+#' @param ... Passed to S3 methods
+#' @param trans_ptr An external pointer to a wk_trans_t transform
+#'   struct.
+#' @param subclass An optional subclass to apply to the pointer
+#' @inheritParams wk_transform
+#'
+#' @export
+#'
 wk_trans_inverse <- function(trans, ...) {
   UseMethod("wk_trans_inverse")
 }
 
+#' @rdname wk_transform
+#' @export
 as_wk_trans <- function(x, ...) {
   UseMethod("as_wk_trans")
 }
 
+#' @rdname wk_transform
+#' @export
 as_wk_trans.wk_trans <- function(x, ...) {
   x
 }
 
+#' @rdname wk_transform
+#' @export
 new_wk_trans <- function(trans_ptr, subclass = character()) {
   stopifnot(typeof(trans_ptr) == "externalptr")
   structure(trans_ptr, class = union(subclass, "wk_trans"))
