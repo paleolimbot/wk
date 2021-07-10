ac0c1c213486470e5eb3c87b424ca09408bf614f
Dewey Dunnington <dewey@fishandwhistle.net>
2021-07-10 15:08:38 -0300

    test set/drop filters

--- R/set.R
+++ R/set.R
@@ -1,20 +1,50 @@
 
-wk_set_z <- function(handleable, z) {
-  wk_set_base(wk_trans_set(xyz(NA, NA, z), use_z = TRUE))
+#' Set coordinate values
+#'
+#' @inheritParams wk_handle
+#' @param z,m A vector of Z or M values applied feature-wise and recycled
+#'    along `handleable`. Use `NA` to keep the existing value of a given
+#'    feature.
+#' @param value An [xy()], [xyz()], [xym()], or [xyzm()] of coordinates
+#'   used to replace values in the input. Use `NA` to keep the existing
+#'   value.
+#' @param use_z,use_m Used to declare the output type. Use `TRUE` to
+#'   ensure the output has that dimension, `FALSE` to ensure it does not,
+#'   and `NA` to leave the dimension unchanged.
+#' @param
+#'
+#' @export
+#'
+#' @examples
+#' wk_set_z(wkt("POINT (0 1)"), 2)
+#' wk_set_m(wkt("POINT (0 1)"), 2)
+#' wk_drop_z(wkt("POINT ZM (0 1 2 3)"))
+#' wk_drop_m(wkt("POINT ZM (0 1 2 3)"))
+#'
+wk_set_z <- function(handleable, z, ...) {
+  wk_set_base(handleable, wk_trans_set(xyz(NA, NA, z), use_z = TRUE), ...)
 }
 
-wk_set_m <- function(handleable, m) {
-  wk_set_base(wk_trans_set(xym(NA, NA, m), use_m = TRUE))
+#' @rdname wk_set_z
+#' @export
+wk_set_m <- function(handleable, m, ...) {
+  wk_set_base(handleable, wk_trans_set(xym(NA, NA, m), use_m = TRUE), ...)
 }
 
-wk_drop_z <- function(handleable) {
-  wk_set_base(wk_trans_set(xy(NA, NA), use_z = FALSE))
+#' @rdname wk_set_z
+#' @export
+wk_drop_z <- function(handleable, ...) {
+  wk_set_base(handleable, wk_trans_set(xy(NA, NA), use_z = FALSE), ...)
 }
 
-wk_drop_m <- function(handleable) {
-  wk_set_base(wk_trans_set(xy(NA, NA), use_m = FALSE))
+#' @rdname wk_set_z
+#' @export
+wk_drop_m <- function(handleable, ...) {
+  wk_set_base(handleable, wk_trans_set(xy(NA, NA), use_m = FALSE), ...)
 }
 
+#' @rdname wk_set_z
+#' @export
 wk_trans_set <- function(value, use_z = NA, use_m = NA) {
   value <- as_xy(value)
   value <- as_xy(value, dims = c("x", "y", "z", "m"))
@@ -24,7 +54,7 @@ wk_trans_set <- function(value, use_z = NA, use_m = NA) {
   )
 }
 
-wk_set_base <- function(handleable, trans) {
-  result <- wk_handle(handleable, wk_transform_filter(wk_writer(handleable), trans))
+wk_set_base <- function(handleable, trans, ...) {
+  result <- wk_handle(handleable, wk_transform_filter(wk_writer(handleable), trans), ...)
   wk_set_crs(wk_restore(handleable, result), wk_crs(handleable))
 }
--- src/transform.c
+++ src/transform.c
@@ -135,13 +135,13 @@ int wk_trans_filter_coord(const wk_meta_t* meta, const double* coord, uint32_t c
     trans_filter->xyzm_in[3] = coord[3];
   } else if (meta->flags & WK_FLAG_HAS_Z) {
     trans_filter->xyzm_in[2] = coord[2];
-    trans_filter->xyzm_in[3] = NA_REAL;
+    trans_filter->xyzm_in[3] = R_NaN;
   } else if (new_meta->flags & WK_FLAG_HAS_M) {
-    trans_filter->xyzm_in[2] = NA_REAL;
+    trans_filter->xyzm_in[2] = R_NaN;
     trans_filter->xyzm_in[3] = coord[2];
   } else {
-    trans_filter->xyzm_in[2] = NA_REAL;
-    trans_filter->xyzm_in[3] = NA_REAL;
+    trans_filter->xyzm_in[2] = R_NaN;
+    trans_filter->xyzm_in[3] = R_NaN;
   }
 
   int result = trans_filter->trans->trans(
