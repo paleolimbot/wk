8e843c0b1542f0f35b9380f7263cddf6bfb4e05b
Dewey Dunnington <dewey@fishandwhistle.net>
2021-08-16 12:00:49 -0300

    better grid constructors

--- R/grid.R
+++ R/grid.R
@@ -2,37 +2,189 @@
 #' Raster-like objects
 #'
 #' @param data An object with two or more dimensions. Most usefully, a matrix.
-#' @param bbox A [rct()] with bounds.
+#' @param bbox A [rct()] containing the bounds and CRS of the object.
+#' @param nx,ny,dx,dy Either a number of cells in the x- and y- directions
+#'   or delta in the x- and y-directions (in which case `bbox` must
+#'   be specified).
+#' @param type Use "polygons" to return a grid whose objects can be
+#'   represented using an [rct()]; use "centers" to return a grid whose
+#'   objects are the center of the [rct()] grid; use "corners" to return
+#'   a grid along the corners of `bbox`.
+#' @param x An object to convert to a grid
+#' @param ... Passed to S3 methods
 #'
 #' @return
+#'   - `wk_grid()` returns a `wk_grid_rct()` for `type == "polygons` or
+#'     a `wk_grid_xy()` otherwise.
+#'   - `wk_grid_rct()` returns an object of class "wk_grid_rct".
+#'   - `wk_grid_xy()` returns an object of class "wk_grid_xy".
 #' @export
 #'
 #' @examples
-#' wk_grid(volcano)
+#' wk_grid_rct(volcano)
 #' # approx bounding box in New Zealand Transverse Mercator
 #' bbox <- wk::rct(5917019, 1756993, 5917466, 1757577, crs = "EPSG:2193")
-#' wk_grid(volcano, bbox)
+#' wk_grid_rct(volcano, bbox)
 #'
+wk_grid <- function(bbox = NULL, nx = NULL, ny = NULL, dx = NULL, dy = NULL,
+                    type = c("polygons", "corners", "centers")) {
+  if (is.null(bbox)) {
+    bbox <- NULL
+  } else if (inherits(bbox, "wk_rct")) {
+    bbox
+  } else {
+    wk_bbox(bbox)
+  }
+  type <- match.arg(type)
+
+  if (is.null(nx) && is.null(ny) && !is.null(bbox)) {
+    rct <- unclass(bbox)
+    width <- rct$xmax - rct$xmin
+    height <- rct$ymax - rct$ymin
+
+    if (type == "polygons") {
+      nx <- width / dx
+      ny <- height / dy
+    } else if (type == "corners") {
+      nx <- width / dx + 1
+      ny <- height / dy + 1
+    } else if (type == "centers") {
+      nx <- width / dx
+      ny <- height / dy
+      bbox <- rct(
+        rct$xmin + dx / 2,
+        rct$ymin + dy / 2,
+        rct$xmax - dx / 2,
+        rct$ymax - dy / 2,
+        crs = wk_crs(bbox)
+      )
+    }
+  } else if (is.null(dx) && is.null(dy)) {
+    if (is.null(bbox)) {
+      bbox <- rct(0, 0, nx, ny)
+    }
+
+    nx <- nx
+    ny <- ny
+
+    if (type == "centers") {
+      rct <- unclass(bbox)
+      width <- rct$xmax - rct$xmin
+      height <- rct$ymax - rct$ymin
+      dx <- width / nx
+      dy <- height / ny
+
+      bbox <- rct(
+        rct$xmin + dx / 2,
+        rct$ymin + dy / 2,
+        rct$xmax - dx / 2,
+        rct$ymax - dy / 2,
+        crs = wk_crs(bbox)
+      )
+    }
+  } else {
+    stop(
+      "Must specify dx, dy, and bbox OR nx and ny.",
+      call. = FALSE
+    )
+  }
+
+  # use a length-zero logical() with correct x and y dims
+  data <- array(dim = c(nx, ny, 0))
+
+  if (type == "polygons") {
+    wk_grid_rct(data, bbox)
+  } else {
+    wk_grid_xy(data, bbox)
+  }
+}
+
+#' @rdname wk_grid
+#' @export
 wk_grid_rct <- function(data, bbox = rct(0, 0, dim(data)[1], dim(data)[2])) {
-  bbox <- as_rct(bbox)
+  bbox <- if (inherits(bbox, "wk_rct")) bbox else wk_bbox(bbox)
   stopifnot(
     length(bbox) == 1,
     length(dim(data)) >= 2
   )
+
+  # with zero values in the xy direction, bbox is empty
+  if ((dim(data)[1] * dim(data)[2]) == 0) {
+    return(
+      new_wk_grid(
+        list(
+          data = data,
+          bbox = wk::wk_bbox(xy(crs = wk_crs(bbox)))
+        ),
+        "wk_grid_rct"
+      )
+    )
+  }
 
   new_wk_grid(list(data = data, bbox = bbox), "wk_grid_rct")
 }
 
-#' @rdname wk_grid_rct
+#' @rdname wk_grid
 #' @export
 wk_grid_xy <- function(data, bbox = rct(0, 0, dim(data)[1], dim(data)[2])) {
-  bbox <- as_rct(bbox)
+  bbox <- if (inherits(bbox, "wk_rct")) bbox else wk_bbox(bbox)
   stopifnot(
     length(bbox) == 1,
     length(dim(data)) >= 2
   )
 
+  # with zero values in the xy direction, bbox is empty
+  if ((dim(data)[1] * dim(data)[2]) == 0) {
+    return(
+      new_wk_grid(
+        list(
+          data = data,
+          bbox = wk::wk_bbox(xy(crs = wk_crs(bbox)))
+        ),
+        "wk_grid_xy"
+      )
+    )
+  }
+
+  # with one value in the x dimension, we need a zero width bbox
+  if (dim(data)[1] == 1) {
+    stopifnot(
+      unclass(bbox)$xmax == unclass(bbox)$xmin
+    )
+  }
+
+  # with one value in the y dimension, we need a zero height bbox
+  if (dim(data)[2] == 1) {
+    stopifnot(
+      unclass(bbox)$ymax == unclass(bbox)$ymin
+    )
+  }
+
   new_wk_grid(list(data = data, bbox = bbox), "wk_grid_xy")
+}
+
+#' @rdname wk_grid
+#' @export
+as_wk_grid_rct <- function(x, ...) {
+  UseMethod("as_wk_grid_rct")
+}
+
+#' @rdname wk_grid
+#' @export
+as_wk_grid_rct.wk_grid_rct <- function(x, ...) {
+  x
+}
+
+#' @rdname wk_grid
+#' @export
+as_wk_grid_xy <- function(x, ...) {
+  UseMethod("as_wk_grid_xy")
+}
+
+#' @rdname wk_grid
+#' @export
+as_wk_grid_xy.wk_grid_xy <- function(x, ...) {
+  x
 }
 
 #' S3 details for grid objects
--- tests/testthat/test-grid.R
+++ tests/testthat/test-grid.R
@@ -3,6 +3,7 @@ test_that("wk_grid_rct() works", {
   grid <- wk_grid_rct(volcano)
   expect_s3_class(grid, "wk_grid_rct")
   expect_s3_class(grid, "wk_grid")
+  expect_identical(as_wk_grid_rct(grid), grid)
   expect_null(wk_crs(grid))
   expect_identical(wk_bbox(grid), rct(0, 0, 87, 61))
 
@@ -14,10 +15,28 @@ test_that("wk_grid_rct() works", {
   expect_output(print(grid_crs), "wk_grid_rct")
 })
 
+test_that("wk_grid_rct() works for an empty grid", {
+  empty <- wk_grid_rct(matrix(nrow = 0, ncol = 0))
+  expect_identical(wk_bbox(empty), wk_bbox(xy(crs = NULL)))
+})
+
+test_that("wk_grid_xy() works for an empty grid", {
+  empty <- wk_grid_xy(matrix(nrow = 0, ncol = 0))
+  expect_identical(wk_bbox(empty), wk_bbox(xy(crs = NULL)))
+})
+
+test_that("wk_grid_xy() works for h/v lines", {
+  hline <- wk_grid_xy(matrix(nrow = 10, ncol = 1), rct(0, 0, 1, 0))
+  expect_identical(wk_bbox(hline), rct(0, 0, 1, 0))
+  vline <- wk_grid_xy(matrix(nrow = 1, ncol = 10), rct(0, 0, 0, 1))
+  expect_identical(wk_bbox(vline), rct(0, 0, 0, 1))
+})
+
 test_that("wk_grid_xy() works", {
   grid <- wk_grid_xy(volcano)
   expect_s3_class(grid, "wk_grid_xy")
   expect_s3_class(grid, "wk_grid")
+  expect_identical(as_wk_grid_xy(grid), grid)
   expect_null(wk_crs(grid))
   expect_identical(wk_bbox(grid), rct(0, 0, 87, 61))
 
@@ -29,3 +48,21 @@ test_that("wk_grid_xy() works", {
   expect_output(print(grid_crs), "wk_grid_xy")
 })
 
+
+test_that("wk_grid() works", {
+  grid_nxny <- wk_grid(nx = 10, ny = 20, type = "polygons")
+  expect_identical(dim(grid_nxny$data), c(10L, 20L, 0L))
+  expect_identical(wk_bbox(grid_nxny), rct(0, 0, 10, 20))
+
+  grid_dxdy <- wk_grid(rct(0, 0, 10, 20), dx = 1, dy = 4)
+  expect_identical(dim(grid_dxdy$data), c(10L, 5L, 0L))
+  expect_identical(wk_bbox(grid_dxdy), rct(0, 0, 10, 20))
+
+  center_nxny <- wk_grid(nx = 10, ny = 20, type = "centers")
+  expect_identical(dim(center_nxny$data), c(10L, 20L, 0L))
+  expect_identical(wk_bbox(center_nxny), rct(0.5, 0.5, 9.5, 19.5))
+
+  center_dxdy <- wk_grid(rct(0, 0, 10, 20), dx = 1, dy = 4, type = "centers")
+  expect_identical(dim(center_dxdy$data), c(10L, 5L, 0L))
+  expect_identical(wk_bbox(center_dxdy), rct(0.5, 2, 9.5, 18))
+})
