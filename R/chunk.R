89d80913024e66aff09609d091262ceb8f0c3ece
Dewey Dunnington <dewey@fishandwhistle.net>
2021-11-08 21:17:30 -0400

    finish chunk strategies

---
+++ R/chunk.R
@@ -0,0 +1,122 @@
+
+#' Chunking strategies
+#'
+#' @param reduce For [wk_chunk_strategy_coordinates()] this refers to
+#'   the function used with [Reduce()] to combine coordinate counts
+#'   from more than one handleable.
+#' @param n_chunks,chunk_size Exactly one of the number of
+#'   chunks or the chunk size. For [wk_chunk_strategy_feature()]
+#'   the chunk size refers to the number of features; for
+#'   [wk_chunk_strategy_coordinates()] this refers to the number
+#'   of coordinates as calculated from multiple handleables
+#'   using `reduce`.
+#'
+#' @return A function that returns a `data.frame` with columns `from` and `to`
+#'   when called with a `handleable` and the feature count.
+#' @export
+#'
+#' @examples
+#' feat <- c(as_wkt(xy(1:4, 1:4)), wkt("LINESTRING (1 1, 2 2)"))
+#' wk_chunk_strategy_single()(list(feat), 5)
+#' wk_chunk_strategy_feature(chunk_size = 2)(list(feat), 5)
+#' wk_chunk_strategy_coordinates(chunk_size = 2)(list(feat), 5)
+#'
+wk_chunk_strategy_single <- function() {
+  function(handleables, n_features) {
+    new_data_frame(list(from = 1, to = n_features))
+  }
+}
+
+#' @rdname wk_chunk_strategy_single
+#' @export
+wk_chunk_strategy_feature <- function(n_chunks = NULL, chunk_size = NULL) {
+  force(n_chunks)
+  force(chunk_size)
+
+  function(handleables, n_features) {
+    chunk_info <- chunk_info(n_features, n_chunks = n_chunks, chunk_size = chunk_size)
+
+    from <- (chunk_info$chunk_size * (seq_len(chunk_info$n_chunks) - 1L)) + 1L
+    to <- chunk_info$chunk_size * seq_len(chunk_info$n_chunks)
+    to[chunk_info$n_chunks] <- n_features
+    new_data_frame(list(from = from, to = to))
+  }
+}
+
+#' @rdname wk_chunk_strategy_single
+#' @export
+wk_chunk_strategy_coordinates <- function(n_chunks = NULL, chunk_size = NULL, reduce = "*") {
+  force(n_chunks)
+  force(reduce)
+
+  function(handleables, n_features) {
+    coord_count <- lapply(handleables, function(handleable) {
+      vm <- wk_vector_meta(handleable)
+      if (identical(vm$geometry_type, 1L)) {
+        1L
+      } else {
+        wk_count(handleable)$n_coord
+      }
+    })
+
+    coord_count <- Reduce(reduce, coord_count)
+    if (identical(coord_count, 1L)) {
+      return(wk_chunk_strategy_feature(n_chunks, chunk_size)(handleables, n_features))
+    }
+
+    coord_count <- rep_len(coord_count, n_features)
+
+    coord_count_total <- sum(coord_count)
+    chunk_info <- chunk_info(coord_count_total, n_chunks, chunk_size)
+
+    from <- rep(NA_integer_, chunk_info$n_chunks)
+    to <- rep(NA_integer_, chunk_info$n_chunks)
+    from[1] <- 1L
+
+    coord_count_chunk <- (coord_count_total / chunk_info$n_chunks)
+    coord_count_feat <- cumsum(coord_count)
+
+    for (chunk_id in seq_len(chunk_info$n_chunks - 1L)) {
+      next_coord_gt <- coord_count_feat >= coord_count_chunk
+      if (!any(next_coord_gt)) {
+        to[chunk_id] <- n_features
+        break
+      }
+
+      i <- max(min(which(next_coord_gt)), from[chunk_id] + 1L)
+      to[chunk_id] <- i
+      from[chunk_id + 1L] <- i + 1L
+      coord_count[1:i] <- 0L
+      coord_count_feat <- cumsum(coord_count)
+    }
+
+    valid <- !is.na(from)
+    from <- from[valid]
+    to <- to[valid]
+
+    if (is.na(to[length(to)])) {
+      to[length(to)] <- n_features
+    }
+
+    new_data_frame(list(from = from, to = to))
+  }
+}
+
+chunk_info <- function(n_features, n_chunks = NULL, chunk_size = NULL) {
+  if (is.null(n_chunks) && is.null(chunk_size)) {
+    stop("Must specify exactly one of `n_chunks` or `chunk_size`", call. = FALSE)
+  } else if (is.null(n_chunks)) {
+    n_chunks <- ((n_features - 1L) %/% chunk_size) + 1L
+  } else if (is.null(chunk_size)) {
+    if (n_features == 0) {
+      n_chunks <- 0L
+      chunk_size <- 1L
+    } else {
+      chunk_size <- ((n_features - 1L) %/% n_chunks) + 1L
+    }
+  } else {
+    stop("Must specify exactly one of `n_chunks` or `chunk_size`", call. = FALSE)
+  }
+
+  list(n_chunks = n_chunks, chunk_size = chunk_size)
+}
---
+++ tests/testthat/test-chunk.R
@@ -0,0 +1,59 @@
+
+test_that("single chunk strategy works", {
+  feat <- c(as_wkt(xy(1:4, 1:4)), wkt("LINESTRING (1 1, 2 2)"))
+  expect_identical(
+    wk_chunk_strategy_single()(list(feat), 5),
+    data.frame(from = 1, to = 5)
+  )
+})
+
+test_that("chunk by feature strategy works", {
+  feat <- c(as_wkt(xy(1:4, 1:4)), wkt("LINESTRING (1 1, 2 2)"))
+  expect_identical(
+    wk_chunk_strategy_feature(chunk_size = 2)(list(feat), 5),
+    data.frame(from = c(1, 3, 5), to = c(2, 4, 5))
+  )
+})
+
+test_that("chunk by coordinates strategy works", {
+  n_coord <- c(1, 5, 1, 5, 1)
+  xs <- unlist(lapply(n_coord, seq_len))
+  ys <- unlist(lapply(n_coord, seq_len))
+  id <- vctrs::vec_rep_each(seq_along(n_coord), n_coord)
+  feat <- wk_linestring(xy(xs, ys), feature_id = id)
+
+  expect_identical(
+    wk_chunk_strategy_coordinates(chunk_size = 6)(list(feat), length(n_coord)),
+    data.frame(from = c(1L, 3L, 5L), to = c(2L, 4L, 5L))
+  )
+})
+
+test_that("chunk_info() works", {
+  expect_identical(
+    chunk_info(5, chunk_size = 2),
+    list(n_chunks = 3, chunk_size = 2)
+  )
+  expect_identical(
+    chunk_info(5, chunk_size = 5),
+    list(n_chunks = 1, chunk_size = 5)
+  )
+  expect_identical(
+    chunk_info(0, chunk_size = 5),
+    list(n_chunks = 0, chunk_size = 5)
+  )
+
+  expect_identical(
+    chunk_info(5, n_chunks = 3),
+    list(n_chunks = 3, chunk_size = 2)
+  )
+  expect_identical(
+    chunk_info(5, n_chunks = 1),
+    list(n_chunks = 1, chunk_size = 5)
+  )
+  expect_identical(
+    chunk_info(0, n_chunks = 5),
+    list(n_chunks = 0L, chunk_size = 1L)
+  )
+  expect_error(chunk_info(1), "exactly one")
+  expect_error(chunk_info(1, chunk_size = 1, n_chunks = 1), "exactly one")
+})
