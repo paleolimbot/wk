24d6e64f5bbef69534999d1125444e1fb1a3018b
Dewey Dunnington <dewey@fishandwhistle.net>
2021-11-09 21:31:12 -0400

    test times an each args

--- tests/testthat/test-slice.R
+++ tests/testthat/test-slice.R
@@ -5,6 +5,10 @@ test_that("wk_slice() works", {
     xy(3:5, 3:5)
   )
   expect_identical(
+    wk_slice(xy(1:5, 1:5), 3, 6, times = 3, each = 2),
+    rep(xy(rep(3:5, each = 2), rep(3:5, each = 2)), times = 3)
+  )
+  expect_identical(
     wk_slice(xy(1:5, 1:5), 0, 2),
     xy(1:2, 1:2)
   )
