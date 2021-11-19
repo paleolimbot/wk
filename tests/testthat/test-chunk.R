781b6c70a55572e457313dae42be2fdecaf1765a
Dewey Dunnington <dewey@fishandwhistle.net>
2021-11-18 21:15:08 -0400

    more tests

--- tests/testthat/test-chunk.R
+++ tests/testthat/test-chunk.R
@@ -24,6 +24,11 @@ test_that("chunk map feature works", {
   # invalid inputs
   expect_error(wk_chunk_map_feature(NULL), "must be a list")
   expect_error(wk_chunk_map_feature(list(NULL)), "must be objects with a")
+  expect_error(
+    wk_chunk_map_feature(list(xy(1:2, 1:2), xy(1:3, 1:3)), identity),
+    "must be recycleable to a common length"
+  )
+
   expect_error(wk_chunk_map_feature(xy(1:5, 1:5), "not a function"), "is not TRUE")
   expect_error(
     wk_chunk_map_feature(xy(1:5, 1:5), identity, vector_args = "bad arg"),
@@ -36,6 +41,42 @@ test_that("chunk map feature works", {
   expect_error(
     wk_chunk_map_feature(xy(1:5, 1:5), identity, strategy = "bad arg"),
     "is not TRUE"
+  )
+})
+
+test_that("chunk map feature works with vectorized and non-vectorized args", {
+
+  wk_chunk_map_feature(
+    xy(1, 1),
+    fun = function(x, l, y) {
+      expect_identical(l, letters[1:5])
+      expect_identical(y, "zippity")
+    },
+    vector_args = data.frame(l = letters[1:5]),
+    args = list(y = "zippity"),
+    strategy = wk_chunk_strategy_single()
+  )
+
+  wk_chunk_map_feature(
+    xy(1:3, 1:3),
+    fun = function(x, l, y) {
+      expect_identical(l, letters[1])
+      expect_identical(y, "zippity")
+    },
+    vector_args = data.frame(l = letters[1]),
+    args = list(y = "zippity"),
+    strategy = wk_chunk_strategy_single()
+  )
+
+})
+
+test_that("chunk map feature doesn't expand handleables more than necessary", {
+  wk_chunk_map_feature(
+    xy(1, 1),
+    fun = function(x) {
+      expect_identical(x, xy(1, 1))
+    },
+    strategy = wk_chunk_strategy_single()
   )
 })
 
@@ -65,6 +106,12 @@ test_that("chunk by coordinates strategy works", {
   expect_identical(
     wk_chunk_strategy_coordinates(chunk_size = 6)(list(feat), length(n_coord)),
     data.frame(from = c(1L, 3L, 5L), to = c(2L, 4L, 5L))
+  )
+
+  # for points there's a shortcut for calculating the chunks
+  expect_identical(
+    wk_chunk_strategy_coordinates(chunk_size = 2)(list(xy(1:6, 1:6)), 6),
+    data.frame(from = c(1, 3, 5), to = c(2, 4, 6))
   )
 })
 
