059904e31e7c8f90a25e66b8cf44a66a45606b4b
Dewey Dunnington <dewey@fishandwhistle.net>
2021-12-14 12:52:53 -0400

    add buffer size as argument for wkt (no size)

--- R/handle-wkt.R
+++ R/handle-wkt.R
@@ -9,12 +9,14 @@ wk_handle.wk_wkt <- function(handleable, handler, ..., wkt_parser_buffer_size = 
 #' Test handlers for handling of unknown size vectors
 #'
 #' @inheritParams wk_handle
+#' @param wkt_parser_buffer_size Buffer size to use for the parser. Useful
+#'   for testing the parser.
 #' @export
 #'
 #' @examples
 #' handle_wkt_without_vector_size(wkt(), wk_vector_meta_handler())
 #'
-handle_wkt_without_vector_size <- function(handleable, handler) {
+handle_wkt_without_vector_size <- function(handleable, handler, wkt_parser_buffer_size = 4096) {
   handler <- as_wk_handler(handler)
-  wk_cpp_handle_wkt(handleable, handler, 4096, reveal_size = FALSE)
+  wk_cpp_handle_wkt(handleable, handler, wkt_parser_buffer_size, reveal_size = FALSE)
 }
