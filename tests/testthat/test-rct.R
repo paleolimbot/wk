d43c0cd79508ce4e461003a920e2408100977286
Dewey Dunnington <dewey@fishandwhistle.net>
2022-10-10 22:08:50 -0300

    rct operators

--- tests/testthat/test-rct.R
+++ tests/testthat/test-rct.R
@@ -48,3 +48,97 @@ test_that("rct() propagates CRS", {
   x[1] <- wk_set_crs(x, 1234L)
   expect_identical(wk_crs(x), 1234)
 })
+
+test_that("rct() accessors return the correct values", {
+  x <- rct(0, 1, 2, 4)
+  expect_identical(rct_xmin(x), 0)
+  expect_identical(rct_xmax(x), 2)
+  expect_identical(rct_ymin(x), 1)
+  expect_identical(rct_ymax(x), 4)
+
+  expect_identical(rct_width(x), 2)
+  expect_identical(rct_height(x), 3)
+})
+
+test_that("rct_intersection() works", {
+  expect_identical(
+    rct_intersection(
+      rct(xmin = 0, xmax = 10, ymin = 0, ymax = 10),
+      rct(xmin = 5, xmax = 15, ymin = 5, ymax = 15)
+    ),
+    rct(xmin = 5, xmax = 10, ymin = 5, ymax = 10)
+  )
+
+  expect_identical(
+    rct_intersection(
+      rct(xmin = 0, xmax = 10, ymin = 0, ymax = 10),
+      rct(xmin = 15, xmax = 25, ymin = 15, ymax = 25)
+    ),
+    rct(xmin = NA_real_, xmax = NA_real_, ymin = NA_real_, ymax = NA_real_)
+  )
+
+  expect_identical(
+    rct_intersection(
+      rct(xmin = NA_real_, xmax = 10, ymin = 0, ymax = 10),
+      rct(xmin = 15, xmax = 25, ymin = 15, ymax = 25)
+    ),
+    rct(xmin = NA_real_, xmax = NA_real_, ymin = NA_real_, ymax = NA_real_)
+  )
+})
+
+test_that("rectangle intersector predicate works", {
+  expect_identical(
+    rct_intersects(
+      rct(xmin = 0, xmax = 10, ymin = 0, ymax = 10),
+      rct(xmin = 5, xmax = 15, ymin = 5, ymax = 15)
+    ),
+    TRUE
+  )
+
+  expect_identical(
+    rct_intersects(
+      rct(xmin = 0, xmax = 10, ymin = 0, ymax = 10),
+      rct(xmin = 15, xmax = 25, ymin = 15, ymax = 25)
+    ),
+    FALSE
+  )
+
+  expect_identical(
+    rct_intersects(
+      rct(xmin = NA_real_, xmax = 10, ymin = 0, ymax = 10),
+      rct(xmin = 5, xmax = 15, ymin = 5, ymax = 15)
+    ),
+    NA
+  )
+})
+
+test_that("rectangle contains predicate works", {
+  expect_true(
+    rct_contains(
+      rct(xmin = 0, xmax = 10, ymin = 0, ymax = 10),
+      xy(x = 5, y = 2)
+    )
+  )
+
+  expect_false(
+    rct_contains(
+      rct(xmin = 0, xmax = 10, ymin = 0, ymax = 10),
+      xy(x = 11, y = 2)
+    )
+  )
+
+  expect_false(
+    rct_contains(
+      rct(xmin = 0, xmax = 10, ymin = 0, ymax = 10),
+      xy(x = 5, y = 11)
+    )
+  )
+
+  expect_identical(
+    rct_contains(
+      rct(xmin = 0, xmax = 10, ymin = 0, ymax = 10),
+      xy(x = NA_real_, y = 2)
+    ),
+    NA
+  )
+})
