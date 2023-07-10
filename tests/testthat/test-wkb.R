a1d25488435fbb92c6aa4d06b6b1826a7a712c7c
Anthony North <anthony.jl.north@gmail.com>
2023-07-10 14:45:49 +1000

    remove dplyr check

--- tests/testthat/test-wkb.R
+++ tests/testthat/test-wkb.R
@@ -120,5 +120,4 @@ test_that("vec_equal(wkb) works", {
   expect_equal(vec_proxy_equal.wk_wkb(x), raw_x)
   expect_equal(vctrs::vec_equal(x, x), c(TRUE, TRUE, TRUE))
   expect_equal(vctrs::vec_equal(x[1], x[2]), FALSE)
-  expect_no_error(dplyr::group_by(data.frame(x), x))
 })
